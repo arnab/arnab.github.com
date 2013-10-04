a626841ed6d56d31f9f178afa71f6d176c05e723
Arnab Deka <arnab.deka@gmail.com>
2013-10-04 11:33:48 +0530

    Site updated at 2013-10-04 06:03:48 UTC

--- javascripts/github.js
+++ javascripts/github.js
@@ -1,9 +1,19 @@
 var github = (function(){
+<<<<<<< HEAD
+=======
+  function escapeHtml(str) {
+    return $('<div/>').text(str).html();
+  }
+>>>>>>> Site updated at 2013-10-04 06:03:48 UTC
   function render(target, repos){
     var i = 0, fragment = '', t = $(target)[0];
 
     for(i = 0; i < repos.length; i++) {
+<<<<<<< HEAD
       fragment += '<li><a href="'+repos[i].url+'">'+repos[i].name+'</a><p>'+repos[i].description+'</p></li>';
+=======
+      fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p>'+escapeHtml(repos[i].description||'')+'</p></li>';
+>>>>>>> Site updated at 2013-10-04 06:03:48 UTC
     }
     t.innerHTML = fragment;
   }
@@ -15,10 +25,17 @@ var github = (function(){
         , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
         , success: function(data) {
           var repos = [];
+<<<<<<< HEAD
           if (!data || !data.repositories) { return; }
           for (var i = 0; i < data.repositories.length; i++) {
             if (options.skip_forks && data.repositories[i].fork) { continue; }
             repos.push(data.repositories[i]);
+=======
+          if (!data || !data.data) { return; }
+          for (var i = 0; i < data.data.length; i++) {
+            if (options.skip_forks && data.data[i].fork) { continue; }
+            repos.push(data.data[i]);
+>>>>>>> Site updated at 2013-10-04 06:03:48 UTC
           }
           if (options.count) { repos.splice(options.count); }
           render(options.target, repos);
